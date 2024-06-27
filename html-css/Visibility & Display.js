1515852644cf97e27280d970b7ca3efb357877af
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-06-27 16:47:05 +0530

    Create Visibility & Display.js

---
+++ html-css/Visibility & Display.js
@@ -0,0 +1,7 @@
+<div style="height:20px;background:red"></div>
+<div style="height:20px;background:green;display:none">Nrupul</div>
+<div style="height:20px;background:blue"></div>
+<br/>
+<div style="height:20px;background:red"></div>
+<div style="height:20px;background:green;visibility:hidden">Nrupul</div>
+<div style="height:20px;background:blue"></div>
